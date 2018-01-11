a342d6bee0bf41dde7e99311a08d7ec1ca6f94c0
Dominique <dominique@lade.cc>
2018-01-11 23:28:17 +0100

    initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Tasmota Firmware Updater
+Script um alle eure Sonoff Geräte mit Tasmota Firmware auf einmal zu aktualisieren.
+
+![screenshot](final.JPG)
+
+# Vor- und Nachteile
++ Funktioniert ohne Internet
++ klein und schlank
+o kann nur Updaten; nicht mehr
+- Python benötigt
+
+# Voraussetzungen
+- Python 3
+- selbstcompilierte Firmwares (normal + minimal) mit WIFI_RETRY, euren Wlan-Daten und dem Webserver aktiviert
+
+# Anleitung
+- Abhängigkeiten installieren: "pip install requests"
+- settings.json öffnen und eure Zugangsdaten für jedes Gerät eingeben
+- In den selben Ordner kopiert ihr eure Firmwares und benennt diese nach "firmware.bin" und "firmware-minimal.bin" um
+- Datei "upgrade.py" ausführen und den Anweisungen folgen
---
+++ upgrade.py
@@ -0,0 +1,103 @@
+import requests
+import json
+import time
+
+def readSettings():
+	try:
+		settings = json.load(open('settings.json'))
+		return settings["credentials"]
+	except FileNotFoundError:
+		raise FileNotFoundError(
+			"Could not found the settings.json file.")
+
+
+def printStatus(credentials):
+	print("Host\t\t\tName\tVersion")
+
+	for sonoff in credentials:
+		status = getStatus(sonoff)
+
+		if status:
+			print("%s\t%s\t%s" % (
+				sonoff["host"],
+				status["Status"]["FriendlyName"],
+				status["StatusFWR"]["Version"]
+			))
+		else:
+			print("%s\t%s\t%s" % (
+				sonoff["host"],
+				'-',
+				'-'
+			))
+
+
+def getStatus(credential):
+	host = 'http://' + credential["host"] + '/cm'
+	payload = {
+		'user'    : credential["user"],
+		'password': credential["password"],
+		'cmnd'    : "status 0"
+	}
+
+	for attempt in range(5):
+		try:
+			r = requests.get(host, params=payload)
+			return r.json()
+		except:
+			pass
+			#print("Could not establish a connection to the device. Retry.")
+	else:
+		print("Something went wrong with device %s" % credential["host"])
+		return False
+
+
+def bulkUpdate(credentials, firmware):
+	for credential in credentials:
+		sendUpdate(credential["host"], firmware)
+
+
+def sendUpdate(host, firmware):
+	url = 'http://' + host + '/u2'
+
+	try:
+		files = {'file': open(firmware + '.bin', 'rb')}
+	except FileNotFoundError:
+		raise FileNotFoundError(
+			"Please make sure that {0} is in the same folder next to this "
+			"script.".format(firmware))
+
+	for attempt in range(5):
+		try:
+			r = requests.post(url, files=files)
+		except:
+			print("Could not establish a connection to the device. Retry.")
+		else:
+			break
+	else:
+		raise (ConnectionError, "Something went wrong with device {0}!".format(host))
+
+
+## Load Settings
+credentials = readSettings()
+
+## Show Status
+printStatus(credentials)
+input("Is everything correct? If you want to continue press ENTER.")
+
+## Flash Minimal Version
+print("Let's start with the minimal firmware. This may take a few minutes.")
+bulkUpdate(credentials, 'firmware-minimal')
+
+## Show Status
+time.sleep(10)
+printStatus(credentials)
+input("Is everything correct? If you want to continue press ENTER.")
+
+## Flash Regular Version
+print("Let's start with the regular firmware. This may take a few minutes.")
+bulkUpdate(credentials, 'firmware')
+
+## Show Status
+time.sleep(10)
+printStatus(credentials)
+input("Press Enter to finish...")
